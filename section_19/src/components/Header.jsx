aa156cce93c768a63b3e6312f25235dfb8cd90bf
Freel11 <jadefreeljd@gmail.com>
2025-01-14 11:49:44 -0500

    Continued work on section 19

---
+++ section_19/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
+
+export default function Header() {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartItems = cartCtx.items.reduce((acc, item) => {
+    return acc + item.quantity;
+  }, 0);
+
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="ReactFood Logo" />
+        <h1>ReactFood</h1>
+      </div>
+      <nav>
+        <Button textOnly>Cart ({totalCartItems})</Button>
+      </nav>
+    </header>
+  );
+}
