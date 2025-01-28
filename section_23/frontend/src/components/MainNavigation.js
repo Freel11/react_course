4d0100ddcb4cc6111aa7ad57c448dd225e4c113b
Freel11 <jadefreeljd@gmail.com>
2025-01-28 13:45:40 -0500

    Finished section 23

--- section_23/frontend/src/components/MainNavigation.js
+++ section_23/frontend/src/components/MainNavigation.js
@@ -1,9 +1,11 @@
-import { Form, NavLink } from "react-router-dom";
+import { Form, NavLink, useRouteLoaderData } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 import NewsletterSignup from "./NewsletterSignup";
 
 function MainNavigation() {
+  const token = useRouteLoaderData("root");
+
   return (
     <header className={classes.header}>
       <nav>
@@ -29,16 +31,18 @@ function MainNavigation() {
               Events
             </NavLink>
           </li>
-          <li>
-            <NavLink
-              to="/auth?mode=login"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              Authentication
-            </NavLink>
-          </li>
+          {!token && (
+            <li>
+              <NavLink
+                to="/auth?mode=login"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Authentication
+              </NavLink>
+            </li>
+          )}
           <li>
             <NavLink
               to="/newsletter"
@@ -49,14 +53,16 @@ function MainNavigation() {
               Newsletter
             </NavLink>
           </li>
-          <li>
-            <Form
-              action="/logout"
-              method="post"
-            >
-              <button>Logout</button>
-            </Form>
-          </li>
+          {token && (
+            <li>
+              <Form
+                action="/logout"
+                method="post"
+              >
+                <button>Logout</button>
+              </Form>
+            </li>
+          )}
         </ul>
       </nav>
       <NewsletterSignup />
