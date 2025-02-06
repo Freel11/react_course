e8c20959d8f0ffcff95fa229dd00d615c1f2887d
Freel11 <jadefreeljd@gmail.com>
2025-02-06 15:35:39 -0500

    Finished section 26 part 2 and started section 27

--- section_26_2/app/meals/[mealSlug]/page.js
+++ section_26_2/app/meals/[mealSlug]/page.js
@@ -3,6 +3,19 @@ import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
 
@@ -25,7 +38,7 @@ export default function MealDetailsPage({ params }) {
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
           <p className={classes.creator}>
-            by <a href={`mailto:${meal.creator_email}`}>Name</a>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
           </p>
           <p className={classes.summary}>{meal.summary}</p>
         </div>
