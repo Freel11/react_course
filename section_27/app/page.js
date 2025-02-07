665ec5177beddb45c74069e6ae546861fb22e4b8
Freel11 <jadefreeljd@gmail.com>
2025-02-07 10:21:03 -0500

    Finished section 27

--- section_27/app/page.js
+++ section_27/app/page.js
@@ -1,7 +1,33 @@
-export default function Home() {
+import ClientDemo from "@/components/ClientDemo";
+import DataFetchingDemo from "@/components/DataFetchingDemo";
+import RSCDemo from "@/components/RSCDemo";
+import ServerActionsDemo from "@/components/ServerActionsDemo";
+import UsePromiseDemo from "@/components/UsePromisesDemo";
+import { Suspense } from "react";
+import fs from "node:fs/promises";
+import { ErrorBoundary } from "@/components/ErrorBoundary";
+
+export default async function Home() {
+  const fetchUsersPromise = new Promise((resolve, reject) =>
+    setTimeout(async () => {
+      const data = await fs.readFile("dummy-db.json", "utf-8");
+      const users = JSON.parse(data);
+      resolve(users);
+    }, 2000)
+  );
+
   return (
     <main>
-      <p>Let's go!</p>
+      {/* <ClientDemo>
+        <RSCDemo />
+      </ClientDemo>
+      <DataFetchingDemo /> */}
+      {/* <ServerActionsDemo /> */}
+      <ErrorBoundary fallback={<p>Something went wrong!</p>}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <UsePromiseDemo usersPromise={fetchUsersPromise} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
